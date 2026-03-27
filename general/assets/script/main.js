46c2aa33b969ad1293a13cf713ab28dc4553535f
ashish sood <ashishsood1998@gmail.com>
2026-03-26 21:55:26 -0500

    main code updated for subscriber

---
+++ general/assets/script/main.js
@@ -0,0 +1,13 @@
+import Subscriber from "./classes/Subscriber.js";
+
+export const currentAccount = new Subscriber(
+  1,
+  "Ashish Sood",
+  "ashishsooddev",
+  "ashishsood@gmail.com",
+  ["Fakebook"],
+  ["JS Developers"],
+  true
+);
+
+export const postsData = [];
